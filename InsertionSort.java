f7fbbfb2f5ee50ec0814cbcc9c19cb7c240daa79
GRy82 <gwryan@buffalo.edu>
2021-03-24 14:09:27 -0400

    more simple form for sort method for InsertionSort, which does not require an additional helper method, was implemented.

--- InsertionSort.java
+++ InsertionSort.java
@@ -1,21 +1,14 @@
 public class InsertionSort {
     public void sort(int[] numsArray){
         int length = numsArray.length;
-        int firstUnsortedIndex = 1;
-        for(int i = 0; i < length - 1; i++){
-            if(numsArray[i] > numsArray[firstUnsortedIndex++])
-                shift(numsArray, firstUnsortedIndex-1);
-        }
-
-    }
-
-    private void shift(int[] numsArray, int lastSortedIndex){
-        for(int i = lastSortedIndex; i > 0; i--){
-            if(numsArray[i-1] < numsArray[i]) return;    
-            
-            int temp = numsArray[i - 1];
-            numsArray[i - 1] = numsArray[i];
-            numsArray[i] = temp; 
+        for(int i = 1; i < length; i++){
+            var current = numsArray[i];
+            int j = i - 1;
+            while(j >= 0 && numsArray[j] > current){
+                numsArray[i] = numsArray[j];
+                j--;
+            }
+            numsArray[j + 1] = current;
         }
     }
 }
