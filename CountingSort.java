6fe2acddc04876129221eef13b036e9834474c24
GRy82 <gwryan@buffalo.edu>
2021-03-25 17:59:05 -0400

    Some modification was made to sort method of CountingSort class to clean it up and make it more concise in its implementation.

--- CountingSort.java
+++ CountingSort.java
@@ -1,16 +1,12 @@
 public class CountingSort {
-    public void sort(int[] numsArray, int k){
-        int[] tallyArray = new int[k + 1];
+    public void sort(int[] numsArray, int max){
+        int[] tallyArray = new int[max + 1];
         for(var num : numsArray)
             tallyArray[num]++;
 
-        for(int i = 0, j = 0; i < numsArray.length; i++){
-            while(tallyArray[j] > 0){
-                numsArray[i++] = j;
-                tallyArray[j]--;
-            } 
-            if(tallyArray[j] == 0) i--;
-            j++;
-        }
+        var k = 0;
+        for(int i = 0; i < tallyArray.length; i++)
+            for(var j = 0; j < tallyArray[i]; j++)
+                numsArray[k++] = i;
     }
 }
--- Main.java
+++ Main.java
@@ -3,7 +3,7 @@ import java.util.Arrays;
 public class Main {
 
     public static void main(String[] args){
-        int[] numsArray = { 5, 4, 3, 3, 5, 4, 2, 1, 0, 5, 4 };
+        int[] numsArray = { 3, 5, 4, 4, 2, 0, 1 };
         //var sorter = new BubbleSort();
         //var sorter = new SelectionSort();
         //var sorter = new InsertionSort();
